8da028c215478d09e9f4e4a4578b1fbaa17dee38
akshit5230 <myfanakki@gmail.com>
2025-08-20 20:44:45 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/akshit5230.github.io' : '',
+  assetPrefix: isProd ? '/akshit5230.github.io/' : '',
 };
 
 export default nextConfig;
